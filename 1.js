fa6f0ee11a0100918ad69ee127d1b6c28f43881c
chenmianhan <mianhan1998@139.com>
2020-06-23 01:50:46 +0800

    finished

---
+++ 1.js
@@ -0,0 +1,10 @@
+var user=null;
+user={}
+user["name"]="John";
+//console.log(user);
+user["surname"]="Mike";
+//console.log(user);
+user.name="Peter";
+//console.log(user);
+delete user.name;
+//console.log(user);
